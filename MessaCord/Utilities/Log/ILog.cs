08efc1fda0753f3ae072025314da2a27a573b28c
Shiglet <sadikano10@gmail.com>
2018-12-09 22:56:31 +0100

    Fixed Log gitignore

---
+++ MessaCord/Utilities/Log/ILog.cs
@@ -0,0 +1,17 @@
+﻿namespace MessaCord.Utilities.Log
+{
+    public interface ILog
+    {
+        void Log(LogLevel logLevel, string log);
+        void Log(string log);
+    }
+
+    public enum LogLevel : int
+    {
+        Default = 0,
+        Debug = 1,
+        Warning = 2,
+        Error = 3
+    }
+
+}
---
+++ MessaCord/Utilities/Log/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessaCord.Utilities.Log
+{
+    public  class Logger : ILog
+    {
+        public Logger(bool debug)
+        {
+            this._debug = debug;
+        }
+
+        private bool _debug = false;
+        public void Log(LogLevel logLevel, string log)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Default:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[Default] "+ log);
+                    Console.ResetColor();
+                    break;
+                case LogLevel.Debug:
+                    if (!_debug) break;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("[Common] " + log);
+                    Console.ResetColor();
+                    break;
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[Warning] " + log);
+                    Console.ResetColor();
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("[Error] " + log);
+                    Console.ResetColor();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
+            }
+        }
+
+        public void Log(string log)
+        {
+            Log(LogLevel.Default, log);
+        }
+
+        public void LogDebug(string log)
+        {
+            Log(LogLevel.Debug, log);
+        }
+
+        public void LogError(string log)
+        {
+            Log(LogLevel.Error, log);
+        }
+    }
+}
